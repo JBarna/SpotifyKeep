956e06d92be76302d998fa315becbe96cc28718d
Joel Barna <joel.tbarna@gmail.com>
2017-08-07 23:46:53 -0400

    Adding in the TRAYZZZ

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const {app} = require('electron'),
+const {app, Menu, Tray} = require('electron'),
     Lib = require('./lib');
 
 // create our debug function if first command line argument is 'debug'
 console.debug = process.argv[2] === 'debug' ? console.log.bind(null, "DEBUG") : Function.prototype;
+
 
 // start our app
 app.on('ready', () => {
     Lib.OAuthManager.getBearerToken().then(console.debug.bind(null, 'Initial Access Token'))
         .catch(console.debug.bind(null, 'Error receiving first bearerToken'));
         
-    //Lib.Hotkey(Lib.SaveSong);
+    Lib.Hotkey(Lib.SaveSong);
+    setInterval(Lib.Playlist, 1000 * 60 * 5);
+    Lib.Tray();
     Lib.Playlist();
 });
