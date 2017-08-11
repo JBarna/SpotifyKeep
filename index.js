de00e902e3dc2ea08ac9a8964b4e115ff85b805b
Joel Barna <joel.tbarna@gmail.com>
2017-08-10 21:02:21 -0400

    We wait until the OAuth process until we start the playlist and keyboard capturing

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ console.debug = process.argv[2] === 'debug' ? console.log.bind(null, "DEBUG") : 
 
 // start our app
 app.on('ready', () => {
-    Lib.OAuthManager.getBearerToken().then(console.debug.bind(null, 'Initial Access Token'))
-        .catch(console.debug.bind(null, 'Error receiving first bearerToken'));
-        
-    Lib.Hotkey(Lib.SaveSong);
-    setInterval(Lib.Playlist, 1000 * 60 * 10);
+    Lib.OAuthManager.getBearerToken().then(token => {
+        console.debug('Initial Access Token', token);
+        Lib.Hotkey(Lib.SaveSong);
+        setInterval(Lib.Playlist, 1000 * 60 * 10);
+        Lib.Playlist();
+    })
+    .catch(console.debug.bind(null, 'Error receiving bearerToken'));
     Lib.Tray();
-    Lib.Playlist();
 });
 
+// prevents the shutdown of our application
+// when the user closes our browser window
 app.on('window-all-closed', Function.prototype);
