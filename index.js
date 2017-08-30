db05e52d80fb8be7850288997f6640d1d97b7ff8
Joel Barna <joel.tbarna@gmail.com>
2017-08-29 23:43:27 -0400

    well boys, I think this puppy is ready for the real world.

--- index.js
+++ index.js
@@ -6,16 +6,29 @@ console.debug = process.argv[2] === 'debug' ? console.log.bind(null, "DEBUG") : 
 
 // start our app
 app.on('ready', () => {
-    Lib.OAuthManager.getBearerToken().then(token => {
-        console.debug('Initial Access Token', token);
-        Lib.Hotkey(Lib.SaveSong.save);
-        setInterval(Lib.Playlist.repeat, 1000 * 60 * 10);
-        Lib.SaveSong.init();
-        Lib.Playlist.repeat();
-    })
-    .catch(console.debug.bind(null, 'Error receiving bearerToken'));
+    Lib.OAuthManager.getBearerToken()
+        .then(() => Promise.all([fetchUserData(), Lib.SaveSong.init()]))
+        .then(() => {
+            Lib.Hotkey(Lib.SaveSong.save);
+            setInterval(Lib.Playlist, 1000 * 60 * 10);
+            Lib.Playlist();
+        })
+        .catch(console.debug.bind(null, 'Error receiving bearerToken or userInfo'));
     Lib.Tray();
 });
+
+function fetchUserData() {
+    // only load this information if we don't have it
+    if (Lib.State.get(Lib.State.keys.USER_ID) == null) {
+        return Lib.HttpsHelper.buildUrlOptions()
+            .then(userUrlOptions => {
+                userUrlOptions.path = '/v1/me';
+                return Lib.HttpsHelper.send(userUrlOptions);
+            }).then(userInfo => {
+                Lib.State.set(Lib.State.keys.USER_ID, userInfo.id);
+            });
+    }
+}
 
 // prevents the shutdown of our application
 // when the user closes our browser window
