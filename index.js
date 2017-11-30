208e964e25254243cd1d55fb4a88f2d454a1c1e9
Barna, Joel <joel.barna@Kronos.com>
2017-11-30 15:39:06 -0500

    changes for index

--- index.js
+++ index.js
@@ -5,28 +5,30 @@ const {app, Menu, Tray} = require('electron'),
 console.debug = process.argv[2] === 'debug' ? console.log.bind(null, "DEBUG") : Function.prototype;
 
 // start our app
-app.on('ready', () => {
-    Lib.OAuthManager.getBearerToken()
-        .then(() => Promise.all([fetchUserData(), Lib.SaveSong.init()]))
-        .then(() => {
-            Lib.Hotkey(Lib.SaveSong.save);
-            setInterval(Lib.Playlist, 1000 * 60 * 10);
-            Lib.Playlist();
-        })
-        .catch(console.debug.bind(null, 'Error receiving bearerToken or userInfo'));
+app.on('ready', async () => {
+
+    try {
+        await Lib.OAuthManager.getBearerToken();
+        await fetchUserData();
+        await Lib.SaveSong.init();
+    } catch(err) {
+        console.debug('Error receiving bearerToken or userInfo', err);
+    }
+
+    Lib.Hotkey(Lib.SaveSong.save);
+    setInterval(Lib.Playlist, 1000 * 60 * 10);
+    Lib.Playlist();
     Lib.Tray();
 });
 
-function fetchUserData() {
+async function fetchUserData() {
     // only load this information if we don't have it
     if (Lib.State.get(Lib.State.keys.USER_ID) == null) {
-        return Lib.HttpsHelper.buildUrlOptions()
-            .then(userUrlOptions => {
-                userUrlOptions.path = '/v1/me';
-                return Lib.HttpsHelper.send(userUrlOptions);
-            }).then(userInfo => {
-                Lib.State.set(Lib.State.keys.USER_ID, userInfo.id);
-            });
+        let userUrlOptions = await Lib.HttpsHelper.buildUrlOptions()
+        userUrlOptions.path = '/v1/me';
+
+        let userInfo = await Lib.HttpsHelper.send(userUrlOptions);
+        Lib.State.set(Lib.State.keys.USER_ID, userInfo.id);
     }
 }
 
