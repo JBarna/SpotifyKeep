cec29cb3565660fc483ae3e9b1f07bf266f42d59
Joel Barna <joel.tbarna@gmail.com>
2022-09-06 21:24:01 -0400

    update packager

--- package.js
+++ package.js
@@ -13,14 +13,6 @@ var options = {
   ignore: [/\/token.json/, /\/ApplicationState.json/, /\/thumb.*/],
 };
 
-// Dynamic album images only work with the beta now on windows
-if (process.platform === "win32") {
-  options.download = {
-    cache: path.join(__dirname, ".."),
-  };
-  options.electronVersion = "1.8.2-beta.2";
-}
-
 packager(options, (err, paths) => {
   if (err) console.log("Error creating package", err);
   else console.log("Success creating package", paths);
